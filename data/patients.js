0bd3092a3a35d5200b55e32e1d0af8791c34c62d
ezwendel <ezwendel@gmail.com>
2021-07-15 15:01:42 -0400

    added update function to db

--- data/patients.js
+++ data/patients.js
@@ -74,7 +74,45 @@ async function add(formRequestBody) {
 }
 
 async function update(id, formRequestBody) {
+    if (!id) throw 'Error: id not given.'
+    if (typeof(id) != "string") throw 'Error: type of id not string.'
+    if (id.trim().length == 0) throw 'Error: id is either an empty string or just whitespace.'
     
+    if (!formRequestBody) throw 'No form body provided'
+    let Patient = require('../models/new_chims_models/case/patient/patient');
+
+    let newPatient = new User();
+
+    newPatient.medicalRef = formRequestBody.medicalRef;
+    newPatient.firstName = formRequestBody.firstName;
+    newPatient.middleInitial = formRequestBody.middleInitial;
+    newPatient.lastName = formRequestBody.lastName;
+    newPatient.guardianID = formRequestBody.guardianID;
+    newPatient.guardianRelationship = formRequestBody.guardianRelationship;
+    newPatient.guardianStaffID = formRequestBody.guardianStaffID;
+    newPatient.SSN = formRequestBody.SSN;
+    newPatient.DOB = formRequestBody.DOB;
+    newPatient.gender = formRequestBody.gender;
+    newPatient.legalStatus = formRequestBody.legalStatus;
+    newPatient.primaryLanguage = formRequestBody.primaryLanguage;
+    newPatient.address = formRequestBody.address;
+    newPatient.address2 = formRequestBody.address2;
+    newPatient.city = formRequestBody.city;
+    newPatient.countyID = formRequestBody.countyID; 
+    // country is missing from form, so don't have it currently
+    newPatient.stateID = formRequestBody.stateID;
+    newPatient.zipCode = formRequestBody.zipCode;
+    newPatient.phone = formRequestBody.phone;
+    newPatient.phone2 = formRequestBody.phone2;
+    newPatient.mobile = formRequestBody.mobile;
+    newPatient.email = formRequestBody.email;
+    newPatient.specialNeeds = formRequestBody.specialNeeds;
+
+    const currentCase = await caseCollection.findOneAndUpate(
+        { "patients._id": ObjectId(id) }, 
+        { $set: newPatient }); // think this should work
+
+    return currentCase;
 }
 
 async function getAllPatients(id) {
