cd89f1b42c83c6f32cc1134695a0427e20bbedac
ezwendel <ezwendel@gmail.com>
2021-07-21 17:23:42 -0400

    added newCase schema, basic case db functions, addCase routing start

--- routes/addcase.js
+++ routes/addcase.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const caseFunctions = require('../data/cases');
+const caseData = require('../data/cases');
 
 const scripts = [
   { script: '/public/js/setCurrentDate.js' }];
@@ -20,7 +20,49 @@ router.get('/', async (req, res, next) => {
 });
 
 router.post('/submitIntake', async (req, res) => {
-  await caseFunctions.addCase(req.body);
+  let caseBody = {};
+
+  caseBody.caseworkerNameCPP = req.body.caseworkerNameCPP;
+  caseBody.caseworkerNumberCPP = req.body.caseworkerNumberCPP;
+  caseBody.caseworkerEmailCPP = req.body.caseworkerEmailCPP;
+  caseBody.supervisorNameCPP = req.body.supervisorNameCPP;
+  caseBody.supervisorNumberCPP = req.body.supervisorNumberCPP;
+  caseBody.supervisorEmailCPP = req.body.supervisorEmailCPP;
+  caseBody.rgName = req.body.rgName;
+  caseBody.rgNumber = req.body.rgNumber;
+  caseBody.rgEmail = req.body.rgEmail;
+  caseBody.officeName = req.body.officeName;
+  caseBody.officeAddress = req.body.officeAddress;
+  caseBody.officeFaxNumber = req.body.officeFaxNumber;
+  caseBody.dateSubmitted = req.body.dateSubmitted;
+  caseBody.caseName = req.body.caseName;
+  caseBody.spiritCaseID = req.body.spiritCaseID;
+  caseBody.referredMemberName = req.body.referredMemberName;
+  caseBody.spiritPersonID = req.body.spiritPersonID;
+
+  let newCase; 
+  try {
+    let newCase = await caseData.add(caseBody);
+  } catch (e) {
+    res.status(500).render('error');
+  }
+
+  let caseId = newCase._id.toString();
+
+  if (!Array.isArray(req.body.servicesRequested)) {
+    req.body.servicesRequested = [req.body.servicesRequested]
+  }
+
+  for (i of req.body.servicesRequested) {
+    let serviceBody = { caseId: caseId, service: i };
+  }
+
+  let patientBody = {};
+
+  patientBody.firstName = req.body.patientFirstName;
+  patientBody.lastName = req.body.patientLastName;
+  patientBody.middleInitial = req.body.patientMiddleInitial;
+
   console.log(req.body);
 });
 
