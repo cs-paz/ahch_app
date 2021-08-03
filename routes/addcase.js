e33c2ab1922d2c50d498f1882b0631111f2ca69e
ezwendel <ezwendel@gmail.com>
2021-08-03 14:45:13 -0400

    finally got the json

--- routes/addcase.js
+++ routes/addcase.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const caseData = require('../data/cases');
 const fs = require('fs');
 const PDFParser = require("pdf2json");
-const Base64 = require('js-base64')
+const Base64 = require('js-base64');
+const multer = require('multer');
+const upload = multer();
 
 var BASE64_MARKER = ';base64,';
 
@@ -39,78 +41,80 @@ router.get('/', async (req, res, next) => {
 });
 
 
-router.post('/fillIntake', async (req, res) => {
-  console.log("here")
+router.post('/fillIntake', upload.single('form'), async (req, res) => {
   let pdfParser = new PDFParser(); // pdf parsing stuff
-  let pdfAsDataUri;
-  for (i in req.body) {
-    // fs.writeFile("./AHCHFormFilled.txt", i, ()=>{console.log("Done.");});
-    pdfAsDataUri = i.toString().replace('data:application/pdf;base64,', '')
-  }
+  // let pdfAsDataUri;
+  // for (i in req.body) {
+  //   // fs.writeFile("./AHCHFormFilled.txt", i, ()=>{console.log("Done.");});
+  //   pdfAsDataUri = i.toString().replace('data:application/pdf;base64,', '')
+  // }
 
-  // fs.writeFile("./AHCHFormFilled.txt", JSON.stringify(req.body), ()=>{console.log("Done.");});
-  // console.log(req.body)
+  // // fs.writeFile("./AHCHFormFilled.txt", JSON.stringify(req.body), ()=>{console.log("Done.");});
+  // // console.log(req.body)
 
-  // fs.writeFile("./new-text.txt", pdfAsDataUri, ()=>{console.log("Done.");});
-  // let pdfAsArray = convertDataURIToBinary(pdfAsDataUri);
-  console.log(pdfAsDataUri)
-  // fs.writeFile("./AHCHFormFilled.pdf", pdfAsDataUri, 'base64', ()=>{console.log("Done.");});
+  // // fs.writeFile("./new-text.txt", pdfAsDataUri, ()=>{console.log("Done.");});
+  // // let pdfAsArray = convertDataURIToBinary(pdfAsDataUri);
+  // console.log(pdfAsDataUri)
+  // // fs.writeFile("./AHCHFormFilled.pdf", pdfAsDataUri, 'base64', ()=>{console.log("Done.");});
+
+  console.log(req.file);
 
   pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
   pdfParser.on("pdfParser_dataReady", pdfData => {
-  fs.writeFile("./AHCHFormFilled.fields.json", JSON.stringify(pdfParser.getAllFieldsTypes()), ()=>{console.log("Done.");});
+  // fs.writeFile("./AHCHFormFilled.fields.json", JSON.stringify(pdfParser.getAllFieldsTypes()), ()=>{console.log("Done.");});
+    console.log(JSON.stringify(pdfParser.getAllFieldsTypes()));
   });
 
-  pdfParser.parseBuffer(Base64.atob(pdfAsDataUri));
+  pdfParser.parseBuffer(req.file.buffer);
 });
 
-router.post('/submitIntake', async (req, res) => {
-  let caseBody = {};
+// router.post('/submitIntake', async (req, res) => {
+//   let caseBody = {};
 
-  caseBody.caseworkerNameCPP = req.body.caseworkerNameCPP;
-  caseBody.caseworkerNumberCPP = req.body.caseworkerNumberCPP;
-  caseBody.caseworkerEmailCPP = req.body.caseworkerEmailCPP;
-  caseBody.supervisorNameCPP = req.body.supervisorNameCPP;
-  caseBody.supervisorNumberCPP = req.body.supervisorNumberCPP;
-  caseBody.supervisorEmailCPP = req.body.supervisorEmailCPP;
-  caseBody.rgName = req.body.rgName;
-  caseBody.rgNumber = req.body.rgNumber;
-  caseBody.rgEmail = req.body.rgEmail;
-  caseBody.officeName = req.body.officeName;
-  caseBody.officeAddress = req.body.officeAddress;
-  caseBody.officeFaxNumber = req.body.officeFaxNumber;
-  caseBody.dateSubmitted = req.body.dateSubmitted;
-  caseBody.caseName = req.body.caseName;
-  caseBody.spiritCaseID = req.body.spiritCaseID;
-  caseBody.referredMemberName = req.body.referredMemberName;
-  caseBody.spiritPersonID = req.body.spiritPersonID;
+//   caseBody.caseworkerNameCPP = req.body.caseworkerNameCPP;
+//   caseBody.caseworkerNumberCPP = req.body.caseworkerNumberCPP;
+//   caseBody.caseworkerEmailCPP = req.body.caseworkerEmailCPP;
+//   caseBody.supervisorNameCPP = req.body.supervisorNameCPP;
+//   caseBody.supervisorNumberCPP = req.body.supervisorNumberCPP;
+//   caseBody.supervisorEmailCPP = req.body.supervisorEmailCPyP;
+//   caseBody.rgName = req.body.rgName;
+//   caseBody.rgNumber = req.body.rgNumber;
+//   caseBody.rgEmail = req.body.rgEmail;
+//   caseBody.officeName = req.body.officeName;
+//   caseBody.officeAddress = req.body.officeAddress;
+//   caseBody.officeFaxNumber = req.body.officeFaxNumber;
+//   caseBody.dateSubmitted = req.body.dateSubmitted;
+//   caseBody.caseName = req.body.caseName;
+//   caseBody.spiritCaseID = req.body.spiritCaseID;
+//   caseBody.referredMemberName = req.body.referredMemberName;
+//   caseBody.spiritPersonID = req.body.spiritPersonID;
 
-  let newCase; 
-  try {
-    let newCase = await caseData.add(caseBody);
-  } catch (e) {
-    console.log(e);
-    res.status(500).render('error');
-    return;
-  }
+//   let newCase; 
+//   try {
+//     let newCase = await caseData.add(caseBody);
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).render('error');
+//     return;
+//   }
 
-  let caseId = newCase._id.toString();
+//   let caseId = newCase._id.toString();
 
-  if (!Array.isArray(req.body.servicesRequested)) {
-    req.body.servicesRequested = [req.body.servicesRequested]
-  }
+//   if (!Array.isArray(req.body.servicesRequested)) {
+//     req.body.servicesRequested = [req.body.servicesRequested]
+//   }
 
-  for (i of req.body.servicesRequested) {
-    let serviceBody = { caseId: caseId, service: i };
-  }
+//   for (i of req.body.servicesRequested) {
+//     let serviceBody = { caseId: caseId, service: i };
+//   }
 
-  let patientBody = {};
+//   let patientBody = {};
 
-  patientBody.firstName = req.body.patientFirstName;
-  patientBody.lastName = req.body.patientLastName;
-  patientBody.middleInitial = req.body.patientMiddleInitial;
+//   patientBody.firstName = req.body.patientFirstName;
+//   patientBody.lastName = req.body.patientLastName;
+//   patientBody.middleInitial = req.body.patientMiddleInitial;
 
-  console.log(req.body);
-});
+//   console.log(req.body);
+// });
 
 module.exports = router;
